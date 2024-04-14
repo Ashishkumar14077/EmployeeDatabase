22c661de03af616facb6444c141dbf5030908b20
ashish.kumar <ashish.kumar@amantyatech.com>
2024-04-14 22:02:21 +0530

    Added gin routes and handlers

--- src/backend/main.go
+++ src/backend/main.go
@@ -1,44 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"backend/http_common"
 
 	"github.com/gin-gonic/gin"
 )
 
-type hello struct {
-	Name string `json:"name"`
-}
-type resposeMsg struct {
-	Msg string `json:"msg"`
-}
-
-func getHello(c *gin.Context) {
-	myHello := []hello{
-		{Name: "Ashish"},
-	}
-	c.JSON(http.StatusOK, myHello)
-}
-
-func putHello(c *gin.Context){
-	var reveicedHello hello
-	err := c.BindJSON(&reveicedHello)
-	if err != nil {
-		return 
-	}
-	fmt.Printf("Recieved: %+v\n",reveicedHello)
-	var respMsg resposeMsg
-	respMsg.Msg = fmt.Sprintf("Hi %s",reveicedHello.Name) 
-	c.JSON(http.StatusCreated,respMsg)
-}
-
 func main() {
-	router := gin.Default()
-
-	router.GET("/hello", getHello)
-	router.POST("/sayHello",putHello)
+	router := CreateGinRoutes()
 
 	router.Run("localhost:8080")
-	fmt.Println("Hello")
+}
+
+func CreateGinRoutes() *gin.Engine {
+	router := gin.Default()
+	http_common.AddRoutes(router)
+	return router
 }
