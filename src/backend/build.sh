6e3a0054801f3ecf2f8e85b60ef06792f55727b8
ashish.kumar <ashish.kumar@amantyatech.com>
2024-04-14 09:41:53 +0530

    added goCommon and build script

--- src/backend/build.sh
+++ src/backend/build.sh
@@ -1,29 +1,28 @@
-#! /bin/bash
+#!/bin/bash
 
 set -x
 
 SCRIPTPATH=$(cd "$(dirname "$0")"; pwd -P)
 
-export GOPATH="$SCRIPTPATH"/../../../../../../common/goCommon/:"$SCRIPTPATH"/../../../../../../
+export GOPATH="$SCRIPTPATH"/../../common/goCommon/:"$SCRIPTPATH"/../../
 echo $GOPATH
-export ENGINE_GOPATH="$SCRIPTPATH"/../../../../../../
+
+#export ENGINE_GOPATH="$SCRIPTPATH"/../../../../../../
 export GO111MODULE="off"
 type go &>/dev/null
-if (( $? != 0 ))
-then
-        echo "Installing golang"
-        sudo yum install -y golang
-        if (( $? != 0 ))
-        then
-                sudo apt-get install -y golang
-        fi
+if (( $? != 0 )); then
+    echo "Installing golang"
+    sudo yum install -y golang
+    if (( $? != 0 )); then
+        sudo apt-get install -y golang
+    fi
 fi
 
 cd "$SCRIPTPATH"
 
-if [ -f "./bin/main_engine" ] ; then
+if [ -f "./bin/backend" ]; then
         make clean
 fi
 
 make || { echo "Build failed"; exit 1; }
-"$SCRIPTPATH"/tools/install_engine_pre_req.sh || { echo "Build failed"; exit 1; }
+# "$SCRIPTPATH"/tools/install_engine_pre_req.sh || { echo "Build failed"; exit 1; }
--- src/backend/start.sh
+++ src/backend/start.sh
@@ -6,11 +6,11 @@ SCRIPTPATH=$(cd "$(dirname "$0")"; pwd -P)
 
 [ ! -f "$SCRIPTPATH"/bin/backend ] && { echo "ENGINE not built yet"; exit 1; }
 
-[ -f "$SCRIPTPATH"/config/config.json ] && cp -bf "$SCRIPTPATH"/config/*.json "$SCRIPTPATH"/ && (sed -i s/127.0.0.1/$(hostname -I | tr -d ' ')/g "$SCRIPTPATH"/config.json; sed -i 's/\("instanceId"[[:blank:]]*:[[:blank:]]*\)"[^"]*\(.*\)/\1"'$(uuidgen)'\2/g' config.json) #To support config maps
+# [ -f "$SCRIPTPATH"/config/config.json ] && cp -bf "$SCRIPTPATH"/config/*.json "$SCRIPTPATH"/ && (sed -i s/127.0.0.1/$(hostname -I | tr -d ' ')/g "$SCRIPTPATH"/config.json; sed -i 's/\("instanceId"[[:blank:]]*:[[:blank:]]*\)"[^"]*\(.*\)/\1"'$(uuidgen)'\2/g' config.json) #To support config maps
 
-export ENGINE_GOPATH="$SCRIPTPATH"/../../
+# export ENGINE_GOPATH="$SCRIPTPATH"/../../
 
-export SSLKEYLOGFILE=~/ssl_key.txt
+# export SSLKEYLOGFILE=~/ssl_key.txt
 
 if [ ! -z $1 ]
 then
